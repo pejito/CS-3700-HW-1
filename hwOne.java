d13f406dcaf212e526b0c4f42d5b8f9a9a2ed972
Ryan Peji <ryanfpeji@gmail.com>
2020-01-27 00:21:42 -0800

    final change, goodbye msg

--- hwOne.java
+++ hwOne.java
@@ -1,5 +1,5 @@
 public class hwOne{
 	public static void main(String args[]){
-		System.out.println("Hello, my name is Ryan Peji");
+		System.out.println("Goodbye, I finished my CS 3700 HW1");
 	}
 }	
